7e6fe208854743bcc0ed9beb928714f4ae6421d7
AeroStun <24841307+AeroStun@users.noreply.github.com>
2021-01-29 19:37:46 +0100

    [CI] Remove 32bit Boost from Windows build
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- ci/install-boost.ps1
+++ ci/install-boost.ps1
@@ -23,8 +23,8 @@ Set-Content -Path "boost-msvc-$MSVC_VER.nuspec" -Value @"
 	<bugTrackerUrl>https://www.boost.org/development/bugs.html</bugTrackerUrl>
 	<packageSourceUrl>https://github.com/Heteroculturalism/chocolatey-packages</packageSourceUrl>
     <requireLicenseAcceptance>false</requireLicenseAcceptance>
-    <description>Boost provides free portable peer-reviewed C++ libraries. The emphasis is on portable libraries which work well with the C++ Standard Library. This package includes Boost headers and libs compiled with Visual Studio for 32bit and 64bit Windows.</description>
-    <summary>Boost headers and libs compiled with Visual Studio 2019 for 32bit and 64bit Windows.</summary>
+    <description>Boost provides free portable peer-reviewed C++ libraries. The emphasis is on portable libraries which work well with the C++ Standard Library. This package includes Boost headers and libs compiled with Visual Studio for 64bit Windows.</description>
+    <summary>Boost headers and libs compiled with Visual Studio 2019 for 64bit Windows.</summary>
     <releaseNotes>http://www.boost.org/users/history/version_$BOOST_SLUG.html</releaseNotes>
     <copyright>Copyright 2019</copyright>
     <tags>boost c++ msvc msvc-$MSVC_VER</tags>
@@ -35,12 +35,6 @@ Set-Content -Path "boost-msvc-$MSVC_VER.nuspec" -Value @"
 New-Item -Path . -Name "tools" -ItemType 'directory'
 
 Set-Content -Path "tools/chocolateyInstall.ps1" -Value @"
-# Install 32 bit binaries
-Install-ChocolateyPackage ``
-    -packageName 'boost-msvc-$MSVC_VER' ``
-    -installerType 'exe' ``
-    -silentArgs '/VERYSILENT' ``
-    -url 'https://downloads.sourceforge.net/project/boost/boost-binaries/$BOOST_VER/boost_$BOOST_SLUG-msvc-$MSVC_VER-32.exe'
 
 # Install 64 bit binaries
 Install-ChocolateyPackage ``
@@ -64,7 +58,7 @@ foreach(`$binaryDir in $`binaryDirs)
 }
 
 # remove root directory if there are no binary directories
-if (-not (Test-Path "`$installDirRoot\lib32*") -and -not (Test-Path "`$installDirRoot\lib64*")) { rm -Recurse -Force "`$installDirRoot" }
+if (-not (Test-Path "`$installDirRoot\lib64*")) { rm -Recurse -Force "`$installDirRoot" }
 "@
 
 choco install "boost-msvc-$MSVC_VER.nuspec"
