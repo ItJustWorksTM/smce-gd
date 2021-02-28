78ef9597fdc420861332519828a67fedac355b80
Ruthger Dijt <ruthgerdijt@gmail.com>
2021-02-28 19:31:21 +0100

    Util: add unzip function that calls native executables for each major platform

--- project/src/utilities/Util.gd
+++ project/src/utilities/Util.gd
@@ -29,3 +29,16 @@ static func user2abs(path: String) -> String:
 		return path
 	
 	return OS.get_user_data_dir() + "/" + path.substr(7)
+
+# Warning: expects system paths
+static func unzip(file: String, working_dir: String) -> bool:
+	if ! File.new().file_exists(file) || ! Directory.new().dir_exists(working_dir):
+		return false
+	
+	match OS.get_name():
+		"X11", "OSX":
+			return OS.execute("tar", ["-C", working_dir, "-zxvf", file], true) == 0
+		"Windows":
+			return OS.execute("powershell.exe", ["Expand-Archive", "-Path", file, "-DestinationPath", working_dir], true) == 0
+	
+	return false
