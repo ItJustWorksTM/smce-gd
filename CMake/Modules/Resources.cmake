5c218dcebd2b6cf1c919c98d6c717c425fa079d8
AeroStun <24841307+AeroStun@users.noreply.github.com>
2021-05-28 16:41:20 +0200

    [CMake] Reformat
    intent=2
    tab=2
    continuation=4
    
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- CMake/Modules/Resources.cmake
+++ CMake/Modules/Resources.cmake
@@ -16,43 +16,43 @@
 #
 
 macro (setup_smce_resources)
-    set (SMCE_RTRES_DIR "${PROJECT_BINARY_DIR}/RtResources")
-    file (REMOVE_RECURSE "${SMCE_RTRES_DIR}")
-    file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}")
+  set (SMCE_RTRES_DIR "${PROJECT_BINARY_DIR}/RtResources")
+  file (REMOVE_RECURSE "${SMCE_RTRES_DIR}")
+  file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}")
 
-    file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/SMCE/share")
-    file (COPY "${PROJECT_SOURCE_DIR}/CMake/Runtime" DESTINATION "${SMCE_RTRES_DIR}/SMCE/share")
-    file (COPY "${PROJECT_SOURCE_DIR}/CMake/Scripts" DESTINATION "${SMCE_RTRES_DIR}/SMCE/share")
+  file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/SMCE/share")
+  file (COPY "${PROJECT_SOURCE_DIR}/CMake/Runtime" DESTINATION "${SMCE_RTRES_DIR}/SMCE/share")
+  file (COPY "${PROJECT_SOURCE_DIR}/CMake/Scripts" DESTINATION "${SMCE_RTRES_DIR}/SMCE/share")
 
-    file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo")
-    file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo/bin")
-    file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo/share")
+  file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo")
+  file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo/bin")
+  file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo/share")
 
-    file (COPY "${PROJECT_SOURCE_DIR}/share/Ardrivo/sketch_main.cpp" DESTINATION "${SMCE_RTRES_DIR}/Ardrivo/share")
+  file (COPY "${PROJECT_SOURCE_DIR}/share/Ardrivo/sketch_main.cpp" DESTINATION "${SMCE_RTRES_DIR}/Ardrivo/share")
 
-    set (SMCE_RESOURCES_ARK "${PROJECT_BINARY_DIR}/SMCE_Resources.zip")
-    file (GENERATE
-            OUTPUT "${SMCE_RTRES_DIR}/Ardrivo/share/ArdrivoOutputNames.cmake"
-            CONTENT
-[[
-#HSD Generated
-set (ARDRIVO_FILE_NAME "$<TARGET_FILE_NAME:Ardrivo>")
-set (ARDRIVO_LINKER_FILE_NAME "$<TARGET_LINKER_FILE_NAME:Ardrivo>")
-]]
-    )
-    add_custom_command (OUTPUT "${SMCE_RESOURCES_ARK}"
-            COMMAND "${CMAKE_COMMAND}" -E copy "$<TARGET_FILE:Ardrivo>" "${SMCE_RTRES_DIR}/Ardrivo/bin"
-            COMMAND "${CMAKE_COMMAND}" -E copy "$<TARGET_LINKER_FILE:Ardrivo>" "${SMCE_RTRES_DIR}/Ardrivo/bin"
-            COMMAND "${CMAKE_COMMAND}" -E tar cf "${SMCE_RESOURCES_ARK}" --format=zip -- "${SMCE_RTRES_DIR}"
-            DEPENDS
-                Ardrivo
-                "${PROJECT_SOURCE_DIR}/CMake/Runtime/CMakeLists.txt"
-                "${PROJECT_SOURCE_DIR}/CMake/Scripts/ConfigureSketch.cmake"
-                "${PROJECT_SOURCE_DIR}/share/Ardrivo/sketch_main.cpp"
-    )
+  set (SMCE_RESOURCES_ARK "${PROJECT_BINARY_DIR}/SMCE_Resources.zip")
+  file (GENERATE
+      OUTPUT "${SMCE_RTRES_DIR}/Ardrivo/share/ArdrivoOutputNames.cmake"
+      CONTENT
+      [[
+        #HSD Generated
+        set (ARDRIVO_FILE_NAME "$<TARGET_FILE_NAME:Ardrivo>")
+        set (ARDRIVO_LINKER_FILE_NAME "$<TARGET_LINKER_FILE_NAME:Ardrivo>")
+      ]]
+  )
+  add_custom_command (OUTPUT "${SMCE_RESOURCES_ARK}"
+      COMMAND "${CMAKE_COMMAND}" -E copy "$<TARGET_FILE:Ardrivo>" "${SMCE_RTRES_DIR}/Ardrivo/bin"
+      COMMAND "${CMAKE_COMMAND}" -E copy "$<TARGET_LINKER_FILE:Ardrivo>" "${SMCE_RTRES_DIR}/Ardrivo/bin"
+      COMMAND "${CMAKE_COMMAND}" -E tar cf "${SMCE_RESOURCES_ARK}" --format=zip -- "${SMCE_RTRES_DIR}"
+      DEPENDS
+      Ardrivo
+      "${PROJECT_SOURCE_DIR}/CMake/Runtime/CMakeLists.txt"
+      "${PROJECT_SOURCE_DIR}/CMake/Scripts/ConfigureSketch.cmake"
+      "${PROJECT_SOURCE_DIR}/share/Ardrivo/sketch_main.cpp"
+  )
 
-    file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo/include")
-    file (COPY "${PROJECT_SOURCE_DIR}/include/Ardrivo" DESTINATION "${SMCE_RTRES_DIR}/Ardrivo/include")
+  file (MAKE_DIRECTORY "${SMCE_RTRES_DIR}/Ardrivo/include")
+  file (COPY "${PROJECT_SOURCE_DIR}/include/Ardrivo" DESTINATION "${SMCE_RTRES_DIR}/Ardrivo/include")
 
-    add_custom_target (ArdRtRes DEPENDS "${SMCE_RESOURCES_ARK}")
+  add_custom_target (ArdRtRes DEPENDS "${SMCE_RESOURCES_ARK}")
 endmacro ()
--- CMake/Modules/SetupBoost.cmake
+++ CMake/Modules/SetupBoost.cmake
@@ -18,59 +18,59 @@
 add_library (SMCE_Boost INTERFACE)
 set (Boost_USE_STATIC_LIBS True)
 if (MSVC)
-    find_package (Boost 1.74 COMPONENTS atomic filesystem date_time)
+  find_package (Boost 1.74 COMPONENTS atomic filesystem date_time)
 else ()
-    find_package (Boost 1.74 COMPONENTS atomic filesystem)
+  find_package (Boost 1.74 COMPONENTS atomic filesystem)
 endif ()
 if (Boost_FOUND)
-    target_link_libraries (SMCE_Boost INTERFACE Boost::headers Boost::atomic Boost::filesystem)
-    target_include_directories (SMCE_Boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
-    target_link_directories (SMCE_Boost INTERFACE ${Boost_LIBRARY_DIRS})
+  target_link_libraries (SMCE_Boost INTERFACE Boost::headers Boost::atomic Boost::filesystem)
+  target_include_directories (SMCE_Boost SYSTEM INTERFACE ${Boost_INCLUDE_DIRS})
+  target_link_directories (SMCE_Boost INTERFACE ${Boost_LIBRARY_DIRS})
 else ()
-#   set (Boost_DEBUG True)
-    set (BOOST_ENABLE_CMAKE True)
+  #   set (Boost_DEBUG True)
+  set (BOOST_ENABLE_CMAKE True)
 
-    if (EXISTS "${PROJECT_SOURCE_DIR}/ext_deps/boost")
-        set (boost_SOURCE_DIR ext_deps/boost)
-    else ()
-        message ("Downloading Boost")
-        FetchContent_Declare (Boost
-            GIT_REPOSITORY "https://github.com/boostorg/boost"
-            GIT_TAG "boost-1.75.0"
-        )
-        FetchContent_GetProperties (Boost)
-        if (NOT boost_POPULATED)
-            FetchContent_Populate (Boost)
-        endif ()
-    endif ()
-    add_subdirectory ("${boost_SOURCE_DIR}" "${boost_BINARY_DIR}" EXCLUDE_FROM_ALL)
-
-    target_link_libraries (SMCE_Boost INTERFACE
-            Boost::atomic # Dependency of Interprocess
-            Boost::filesystem # Dependency of Process
-            Boost::intrusive # Dependency of Interprocess
-            Boost::container # Dependency of Interprocess
-            Boost::date_time # Dependency of Interprocess
+  if (EXISTS "${PROJECT_SOURCE_DIR}/ext_deps/boost")
+    set (boost_SOURCE_DIR ext_deps/boost)
+  else ()
+    message ("Downloading Boost")
+    FetchContent_Declare (Boost
+        GIT_REPOSITORY "https://github.com/boostorg/boost"
+        GIT_TAG "boost-1.75.0"
     )
-    target_include_directories (SMCE_Boost SYSTEM INTERFACE
-            "${boost_SOURCE_DIR}/libs/process/include"
-            "${boost_SOURCE_DIR}/libs/interprocess/include"
-            "${boost_SOURCE_DIR}/libs/asio/include" # Dependency of Process
-            "${boost_SOURCE_DIR}/libs/algorithm/include" # Dependency of Interprocess
-            "${boost_SOURCE_DIR}/libs/range/include" # Dependency of Interprocess
-            "${boost_SOURCE_DIR}/libs/numeric/conversion/include" # Dependency of Interprocess
-    )
-
-    if (NOT WIN32 AND NOT APPLE)
-        set_property (TARGET boost_filesystem PROPERTY POSITION_INDEPENDENT_CODE True)
+    FetchContent_GetProperties (Boost)
+    if (NOT boost_POPULATED)
+      FetchContent_Populate (Boost)
     endif ()
+  endif ()
+  add_subdirectory ("${boost_SOURCE_DIR}" "${boost_BINARY_DIR}" EXCLUDE_FROM_ALL)
+
+  target_link_libraries (SMCE_Boost INTERFACE
+      Boost::atomic # Dependency of Interprocess
+      Boost::filesystem # Dependency of Process
+      Boost::intrusive # Dependency of Interprocess
+      Boost::container # Dependency of Interprocess
+      Boost::date_time # Dependency of Interprocess
+  )
+  target_include_directories (SMCE_Boost SYSTEM INTERFACE
+      "${boost_SOURCE_DIR}/libs/process/include"
+      "${boost_SOURCE_DIR}/libs/interprocess/include"
+      "${boost_SOURCE_DIR}/libs/asio/include" # Dependency of Process
+      "${boost_SOURCE_DIR}/libs/algorithm/include" # Dependency of Interprocess
+      "${boost_SOURCE_DIR}/libs/range/include" # Dependency of Interprocess
+      "${boost_SOURCE_DIR}/libs/numeric/conversion/include" # Dependency of Interprocess
+  )
+
+  if (NOT WIN32 AND NOT APPLE)
+    set_property (TARGET boost_filesystem PROPERTY POSITION_INDEPENDENT_CODE True)
+  endif ()
 endif ()
 
 add_library (Boost_ipc INTERFACE)
 if (WIN32)
-    target_link_libraries (Boost_ipc INTERFACE ole32 oleaut32 psapi advapi32)
+  target_link_libraries (Boost_ipc INTERFACE ole32 oleaut32 psapi advapi32)
 elseif (NOT APPLE)
-    target_link_libraries (Boost_ipc INTERFACE rt)
+  target_link_libraries (Boost_ipc INTERFACE rt)
 endif ()
 add_library (Boost::ipc ALIAS Boost_ipc)
 
@@ -78,6 +78,6 @@ target_link_libraries (SMCE_Boost INTERFACE Boost_ipc)
 
 install (CODE "file (INSTALL \"$<TARGET_FILE:Boost::filesystem>\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib64/boost\" FOLLOW_SYMLINK_CHAIN)")
 if (MSVC)
-    install (CODE "file (INSTALL \"$<TARGET_FILE:Boost::atomic>\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib64/boost\" FOLLOW_SYMLINK_CHAIN)")
-    install (CODE "file (INSTALL \"$<TARGET_FILE:Boost::date_time>\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib64/boost\" FOLLOW_SYMLINK_CHAIN)")
+  install (CODE "file (INSTALL \"$<TARGET_FILE:Boost::atomic>\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib64/boost\" FOLLOW_SYMLINK_CHAIN)")
+  install (CODE "file (INSTALL \"$<TARGET_FILE:Boost::date_time>\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib64/boost\" FOLLOW_SYMLINK_CHAIN)")
 endif ()
