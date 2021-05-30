49bea55ad28ca3ac5b09fbfb9c26d6c45e6b0cb6
AeroStun <24841307+AeroStun@users.noreply.github.com>
2021-05-31 01:51:42 +0200

    [Ardrivo] WiFi: fix building in release mode
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- include/Ardrivo/WiFi.h
+++ include/Ardrivo/WiFi.h
@@ -19,24 +19,24 @@
 #ifndef WiFi_h
 #define WiFi_h
 
-#include <cassert>
+#include <cstdint>
 #include "Client.h"
 #include "IPAddress.h"
 #include "SMCE_dll.hpp"
 
 struct WiFiClass : Client {
-    [[noreturn]] inline int connect([[maybe_unused]] IPAddress ip, [[maybe_unused]] uint16_t port) override { assert(false); }
-    [[noreturn]] int connect([[maybe_unused]] const char* host, [[maybe_unused]] uint16_t port) override { assert(false); }
-    [[noreturn]] size_t write([[maybe_unused]] uint8_t) override { assert(false); }
-    [[noreturn]] size_t write([[maybe_unused]] const uint8_t* buf, [[maybe_unused]] size_t size) override { assert(false); }
-    [[noreturn]] int available() override { assert(false); }
-    [[noreturn]] int read() override { assert(false); }
-    [[noreturn]] int read([[maybe_unused]] uint8_t* buf, [[maybe_unused]] size_t size) override { assert(false); }
-    [[noreturn]] int peek() override { assert(false); }
-    [[noreturn]] void flush() override { assert(false); }
-    [[noreturn]] void stop() override { assert(false); }
-    [[noreturn]] uint8_t connected() override { assert(false); }
-    [[noreturn]] operator bool() override { assert(false); }
+    inline int connect([[maybe_unused]] IPAddress ip, [[maybe_unused]] std::uint16_t port) override { return 0; }
+    int connect([[maybe_unused]] const char* host, [[maybe_unused]] std::uint16_t port) override { return 0; }
+    std::size_t write([[maybe_unused]] std::uint8_t) override { return 0; }
+    std::size_t write([[maybe_unused]] const std::uint8_t* buf, [[maybe_unused]] std::size_t size) override { return 0; }
+    int available() override { return 0; }
+    int read() override { return -1; }
+    int read([[maybe_unused]] std::uint8_t* buf, [[maybe_unused]] std::size_t size) override { return 0; }
+    int peek() override { return -1; }
+    void flush() override { }
+    void stop() override { }
+    std::uint8_t connected() override { return 0; }
+    operator bool() override { return false; }
 };
 
 SMCE__DLL_RT_API
