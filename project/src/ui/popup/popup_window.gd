d04b09d6b607851dd9aff8f794a8506fd4d7a842
Anthony1911 <guspathan@student.gu.se>
2021-11-12 23:27:00 +0100

    Refactoring
    Replacing duplicate code with dedicated method

--- project/src/ui/popup/popup_window.gd
+++ project/src/ui/popup/popup_window.gd
@@ -29,16 +29,20 @@ func _ready():
 	buttonYes.connect("pressed", self, "_buttonYes_pressed")
 	buttonNo.text = "No"
 	buttonNo.connect("pressed", self, "_buttonNo_pressed")
-	
+
+# Function to widen the window to support larger text
+func widenWindow(var i):
+	panel.margin_right = 793 + i * 30
+	panel.margin_left = 511 - i * 30
+	msgLabel.margin_right = 782 + i * 30
+	msgLabel.margin_left = 522 - i * 30
+
 # Call this to display informational popup with simple "OK" button
 func info(message):
 	var i = 1
 	if (message.length()>112):
 		i = (message.length()/28)-4
-		panel.margin_right = 793 + i*30
-		panel.margin_left = 511 - i *30
-		msgLabel.margin_right = 782 + i*30
-		msgLabel.margin_left = 522 - i * 30
+		widenWindow(i)
 		msgLabel.text = message
 		buttonYes.set_visible(false)
 		buttonNo.set_visible(false)
@@ -54,14 +58,11 @@ func confirmation(message):
 	var i = 1
 	if (message.length()>112):
 		i = (message.length()/28)-4
-		panel.margin_right = 793 + i*30
-		panel.margin_left = 511 - i *30
-		msgLabel.margin_right = 782 + i*30
-		msgLabel.margin_left = 522 - i * 30
+		widenWindow(i)
 		buttonYes.margin_right = 588 - i * 30
 		buttonYes.margin_left = 491 - i * 30
-		buttonNo.margin_left = 713 + i*30
-		buttonNo.margin_right = 810 + i*30
+		buttonNo.margin_left = 713 + i * 30
+		buttonNo.margin_right = 810 + i * 30
 		msgLabel.text = message
 		buttonOK.set_visible(false)
 		popup()
