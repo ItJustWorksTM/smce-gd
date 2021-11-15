ae77d45d3e20c39767bb42e8ebc0b2d1fcb847ad
akjeller <alfred.kjeller@telia.com>
2021-11-15 16:46:33 +0100

    Moved texteditor code to new file

--- project/src/ui/code_editor/MainWindow.gd
+++ project/src/ui/code_editor/MainWindow.gd
@@ -23,7 +23,7 @@ onready var fileLoader = load("res://src/ui/file_dialog/FileLoader.gd").new()
 func _ready():
 	close_btn.connect("pressed", self, "_on_close")
 	_init_dropdown()
-	_init_TextEditor()
+	textEditor._init_content()
 
 # Initializes the dropdown menu button
 func _init_dropdown():
@@ -33,46 +33,6 @@ func _init_dropdown():
 	dropdown_btn.get_popup().add_item("New File")
 	dropdown_btn.get_popup().add_item("New Arduino-Project")
 	dropdown_btn.get_popup().add_item("Close")
-
-#Initializes the texteditor settings
-func _init_TextEditor():
-	#Standard text
-	if(src_file == null):
-		textEditor.text = "Please open a file to edit"
-	else:
-		_load_content(src_file)
-	
-	#Enable syntax highlightning
-	textEditor.syntax_highlighting = true
-	
-	#Arduino syntax highlighting
-	textEditor.add_color_region('//','',Color(0.638306, 0.65625, 0.65625)) # comments
-	textEditor.add_color_region('/*','*/',Color(0.834412, 0.847656, 0.847656)) # info boxes
-	textEditor.add_color_region('"','"',Color(0.085144, 0.605469, 0.56721)) # Strings
-
-	#variables
-	var varTypes = ['PROGMEM','sizeof','HIGH','LOW','OUTPUT','uint8_t','private','public','class','static','const','float','int','String','uint16_t','boolean','bool','void','byte','unsigned','long','char','uint32_t','word','struct']
-	for v in varTypes:
-		textEditor.add_keyword_color(v,Color(0.228943, 0.945313, 0.844573))
-	
-	#operators/keywords	
-	var operators = ['ifndef','endif ','define','ifdef','include','setup','loop','if','for','while','switch','else','case','break','and','or','final','return']
-	for o in operators:
-		textEditor.add_keyword_color(o,Color(0.605167, 0.875, 0.071777))
-	
-	#stream, serial, other operations
-	var other = ['interrupts','noInterrupts','CAN','setCursor','display','bit','read','peek','onReceive','onRequest','flush', 'requestFrom','endTransmission','beginTransmission','setClock', 'status','write','size_t','Stream','Serial','begin','end','stop','print','printf','println','delay','attach','readMsgBuf','sendMsgBuf']
-	for t in other:
-		textEditor.add_keyword_color(t,Color(0.976563, 0.599444, 0.324249))
-		
-	textEditor.caret_blink = true
-	textEditor.show_line_numbers = true
-	textEditor.add_child(BraceEnabler.new())
-
-	#Minimap view
-	#textEditor.minimap_draw = true
-	#textEditor.minimap_width = 150
-	
 
 # Function that hides the editor when its closed with a dedicated button
 func _on_close() -> void:
@@ -116,7 +76,6 @@ func _new_proj():
 	fileDialog.add_filter("*.ino; ino file")
 	fileDialog.popup()	#Get path for new file
 	fileDialog.clear_filters()
-	
 	
 # Function to collect the path of a selected file and send it to the editor
 func _on_FileDialog_file_selected(path):
