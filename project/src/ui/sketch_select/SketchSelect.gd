e13ac28dfa7c95d2a59d4a075d88038f0b1af7dc
Ruthger Dijt <ruthgerdijt@gmail.com>
2022-01-16 17:19:07 +0100

    Update copyright year to 2022

--- project/src/ui/sketch_select/SketchSelect.gd
+++ project/src/ui/sketch_select/SketchSelect.gd
@@ -1,6 +1,6 @@
 #
 #  SketchSelect.gd
-#  Copyright 2021 ItJustWorksTM
+#  Copyright 2022 ItJustWorksTM
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
