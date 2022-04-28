37b430a62adea5b7bb8917908ae677d8a50fd7df
Ruthger Dijt <ruthgerdijt@gmail.com>
2022-04-28 10:41:15 +0200

    mod_manager: add extra debug prints

--- project/src/ui/master_control/ModManager.gd
+++ project/src/ui/master_control/ModManager.gd
@@ -22,22 +22,28 @@ func load_mods() -> void:
 	var ext_mod_dir = Global.usr_dir_plus("mods")
 	var local_mod_dir = "res://mods"
 	
+	var loaded_count := 0
 	for mod_pck in Util.ls(ext_mod_dir):
+		print("Found mod pck: %s" % mod_pck)
 		if !ProjectSettings.load_resource_pack(ext_mod_dir.plus_file(mod_pck), true):
+			print("Failed to load mod pck")
 			continue
-		
-		print("Loaded mod pck: %s" % mod_pck)
+		loaded_count += 1
 	
+	var initialized_count := 0
 	for mod in Util.ls(local_mod_dir):
+		print("Initializing mod file: ", mod)
 		var path: String = local_mod_dir.plus_file(mod)
 		
 		if !ResourceLoader.exists(path, "GDScript"):
+			printerr("Mod file %s is not a valid script!" % path)
 			continue
 
 		var script = load(path)
 		if ! is_instance_valid(script):
+			printerr("Can not load mod file as GDScript")
 			continue
-			
+		
 		var inst = script.new()
 		
 		if !_is_mod(inst):
@@ -47,12 +53,18 @@ func load_mods() -> void:
 				inst.free()
 			continue
 		
+		print("Mod instance created, named %s" % inst.mod_name)
+		
 		if inst is Node:
 			inst.name = inst.mod_name
 			add_child(inst)
 		
-		print("Initializing mod: %s" % inst.mod_name)
 		inst.init(Global)
+		
+		initialized_count += 1
+	
+	if loaded_count != initialized_count:
+		print("The amount of loaded mods differs from the amount of initialized mods!")
 
 
 func _is_mod(ref) -> bool:
